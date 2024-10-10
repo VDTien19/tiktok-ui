1a32e7d005e63b4932928133ee5a730c6bf3e1d1
tiennehihi <vuductientienti@gmail.com>
2024-10-10 16:30:53 +0700

    Install Tippy và làm phần hiện kết quả tìm kiếm

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,34 +1,68 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import Tippy from '@tippyjs/react/headless';
 
+import { Wrapper as PopperWrapper } from '~/components/Popper'
 import styles from './Header.module.scss'
 import images from '~/assets/images'
-
-// console.log(images.logo);
+import AccountItem from '../AccountItem';
 
 // classname có thể viết '-'
 const cx = classNames.bind(styles)
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            <img src={images.logo} alt="Tiktok" />
-            <div className={cx('search')}>
-                <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
-                <button className={cx('clear')}>
-                    <FontAwesomeIcon icon={faCircleXmark} />
-                </button>
-                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                <button className={cx('search-btn')}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </button>
-            </div>
-            <div className={cx('actions')}>
+    const [searchResult, setSearchResult] = useState([])
 
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([])
+        }, 0)
+    }, [])
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div>
+                    <Tippy
+                        visible={searchResult.length > 0}
+                        interactive
+                        render={attrs => (
+                            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                                <PopperWrapper>
+                                    <h4 className={cx('search-title')}>Accounts</h4>
+                                    <AccountItem/>
+                                    <AccountItem/>
+                                    <AccountItem/>
+                                    <AccountItem/>
+                                </PopperWrapper>
+                            </div>
+                        )}
+                    >
+                        <div className={cx('search')}>
+                            <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+                            <button className={cx('clear')}>
+                                <FontAwesomeIcon icon={faCircleXmark} />
+                            </button>
+                            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                            <button className={cx('search-btn')}>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </button>
+                        </div>
+                    </Tippy>
+                </div>
+                <div className={cx('actions')}>
+
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
+    )
 }
+
+// setTimeout(() => {
+//     debugger;
+// }, 5000)
 
 export default Header;
