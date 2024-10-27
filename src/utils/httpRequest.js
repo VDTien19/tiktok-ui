b4670f866f294a1824a5a6efb0866be1803944dd
tiennehihi <vuductientienti@gmail.com>
2024-10-27 23:16:01 +0700

    Update httpRequest

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -6,19 +6,30 @@ const httpRequest = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
 });
 
+httpRequest.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error),
+);
+
 export const get = async (path, options = {}) => {
     const respone = await httpRequest.get(path, options);
     return respone.data;
 };
 
 export const post = async (path, body = {}, options = {}) => {
-    const respone = await httpRequest.post(path, body, options)
-    return respone.data
-}
+    const respone = await httpRequest.post(path, body, options);
+    return respone.data;
+};
 
 export const deleted = async (path, body = {}, options = {}) => {
-    const respone = await httpRequest.delete(path, body, options)
-    return respone.data
-}
+    const respone = await httpRequest.delete(path, body, options);
+    return respone.data;
+};
 
 export default httpRequest;
