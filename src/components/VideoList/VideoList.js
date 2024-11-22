3cdc6da38a5e428a5b27727bf02654d1750c8159
tiennehihi <vuductientienti@gmail.com>
2024-11-22 14:37:13 +0700

    connect API render ListVideo

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,36 +1,46 @@
 import classNames from 'classnames/bind';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
-import styles from './VideoList.module.scss'
+import styles from './VideoList.module.scss';
 import { getListVideo } from '~/services/videoServices';
-import VideoItem from '~/components/VideoItem'
+import VideoItem from '~/components/VideoItem';
 
 const cx = classNames.bind(styles);
 
 function VideoList() {
     const [videos, setVideos] = useState([]);
-    const [page, setPage] = useState(7);
+    const [page, setPage] = useState(15);
+    const [playingIndex, setPlayingIndex] = useState(null);
+
+    console.log("playingIndex: ", playingIndex)
 
     const fetchVideos = async () => {
         try {
-            const data = await getListVideo('for-you', page)
-            setVideos(prevVideo => [...prevVideo, ...data])
+            const data = await getListVideo('for-you', page);
+            setVideos((prevVideos) => [...prevVideos, ...data]);
         } catch (err) {
-            // setError('Không thể tải danh sách video. Vui lòng thử lại!');
             console.error('Lỗi khi lấy danh sách video: ', err);
-        } finally {
-            // setLoading(false); // Tắt trạng thái loading
         }
-    }
+    };
 
     useEffect(() => {
         fetchVideos();
-    }, [])
+    }, [page]);
 
-    return (  
+    const handlePlaying = (index) => {
+        setPlayingIndex(index);
+    };
+
+    return (
         <div className={cx('wrapper')}>
             {videos.map((video, index) => (
-                <VideoItem key={index} data={video} />
+                <VideoItem
+                    key={video.id}
+                    data={video}
+                    index={index}
+                    onPlaying={handlePlaying}
+                    playingIndex={playingIndex}
+                />
             ))}
         </div>
     );
