6a68fb20864525b0a5f683f6eb132203acc9903e
tiennehihi <vuductientienti@gmail.com>
2024-12-20 14:52:19 +0700

    custom useLike hook to manage likes

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useDebounce } from './useDebounce';
 export { default as useVideoIntersection } from './useVideoIntersection';
+export { default as useLike } from './useLike';
---
+++ src/hooks/useLike.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import * as likeServices from '~/services/likeServices';
+
+const useLike = (initialIsLiked, initialLikeCount, videoId) => {
+    const [isLiked, setIsLiked] = useState(initialIsLiked);
+    const [likeCount, setLikeCount] = useState(initialLikeCount);
+
+    const toggleLike = async () => {
+        try {
+            if(isLiked) {
+                await likeServices.unlikeVideo(videoId);
+                setLikeCount(prev => prev - 1);
+            } else {
+                await likeServices.likeVideo(videoId);
+                setLikeCount(prev => prev + 1);
+            }
+            setIsLiked(!isLiked);
+        } catch (error) {
+            console.error('Error toggling like:', error);
+        }
+    }
+
+    return { isLiked, likeCount, toggleLike };
+}
+
+export default useLike
