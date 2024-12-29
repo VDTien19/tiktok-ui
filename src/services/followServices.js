3497b06971430aff0b9cb639a2e99a1257d0128c
tiennehihi <vuductientienti@gmail.com>
2024-12-30 00:21:32 +0700

    connect API followService

--- src/services/followServices.js
+++ src/services/followServices.js
@@ -14,3 +14,23 @@ export const getFollowing = async ({ page, perPage }) => {
         throw err;
     }
 }
+
+export const followUser = async (idUser) => {
+    try {
+        const respone = await httpRequest.post(`users/${idUser}/follow`, {});
+        return respone;
+    } catch (err) {
+        console.log("Failed to follow user: ", err);
+        throw err;
+    }
+}
+
+export const unFollowUser = async (idUser) => {
+    try {
+        const respone = await httpRequest.post(`users/${idUser}/unfollow`, {});
+        return respone;
+    } catch (err) {
+        console.log("Failed to unfollow user: ", err);
+        throw err;
+    }
+}
