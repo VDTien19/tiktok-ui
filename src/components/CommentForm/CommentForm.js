9e374fc831b1ae6207146738af653fc42eab281d
tiennehihi <vuductientienti@gmail.com>
2025-01-19 01:20:14 +0700

    connect API and logic handle when user create a comment

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,16 +1,70 @@
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react';
+import { useState, useRef } from 'react';
 
-import styles from './CommentForm.module.scss'
+import styles from './CommentForm.module.scss';
 import { MentionIcon, EmojiIcon } from '~/components/Icons';
+import { createComment } from '~/services/commentServices'
 
-const cx = classNames.bind(styles)
-function CommentForm() {
-    return (  
+const cx = classNames.bind(styles);
+function CommentForm({ idVideo, refetchComments }) {
+    const inputRef = useRef(null);
+
+    const [commentValue, setCommentValue] = useState('');
+    const [activeButton, setActiveButton] = useState(false);
+
+    const handleChange = (e) => {
+        const commentValue = e.target.value;
+        
+        if(!commentValue.startsWith(' ')) {
+            setCommentValue(commentValue);
+            setActiveButton(true);
+        }
+    }
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(inputRef.current.value === '') {
+            inputRef.current.focus();
+            return;
+        }
+        try {
+            await createComment(idVideo, commentValue);
+            if(inputRef.current) {
+                inputRef.current.focus();
+                setCommentValue('');
+                setActiveButton(false);
+            }
+            console.log('Comment successfully');
+            await refetchComments();
+        } catch (e) {
+            console.log(e);
+        }
+        // console.log("Comment: " + commentValue);
+    }
+
+    // const prevHandleSubmit = (e) => {
+    //     e.preventDefault();
+    //     if(inputRef.current.value !== '') {
+    //         handleSubmit(e)
+    //     }
+    // };  
+
+    return (
         <div className={cx('wrapper')}>
-            <form className={cx('form-group')} onSubmit={e => e.preventDefault()}>
+            <form
+                className={cx('form-group')}
+                onSubmit={handleSubmit}
+            >
                 <div className={cx('comment-content')}>
-                    <input className={cx('input')} type="text" placeholder='Thêm bình luận...' />
+                    <input
+                        ref={inputRef}
+                        className={cx('input')}
+                        type="text"
+                        placeholder="Thêm bình luận..."
+                        value={commentValue}
+                        onChange={handleChange}
+                    />
                     <div className={cx('option')}>
                         <Tippy
                             content="Dùng ký hiệu '@' để gắn thẻ một người dùng"
@@ -20,17 +74,16 @@ function CommentForm() {
                                 <MentionIcon />
                             </div>
                         </Tippy>
-                        <Tippy
-                            placement='top'
-                            content='Nhấp để thêm Emoji'
-                        >
+                        <Tippy placement="top" content="Nhấp để thêm Emoji">
                             <div className={cx('emoji')}>
                                 <EmojiIcon />
                             </div>
                         </Tippy>
                     </div>
                 </div>
-                <button className={cx('submit-btn')} type='submit'>Đăng</button>
+                <button className={cx('submit-btn', {'btn-active': activeButton})} type="submit">
+                    Đăng
+                </button>
             </form>
         </div>
     );
