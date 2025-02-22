b0d5484a35cd3d5dcf43c5c0c6e7615e29dcfa5f
tiennehihi <vuductientienti@gmail.com>
2025-02-23 00:01:41 +0700

    Logic handle tab in profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,6 @@
 import { useParams } from "react-router-dom";
 import classNames from "classnames/bind";
 
-import { useAuth } from "~/contexts/AuthContext";
 import ProfileHeader from "~/components/ProfileHeader";
 import ProfileTab from "~/components/ProfileTab";
 import CreatorVideo from "~/components/CreatorVideo";
@@ -11,15 +10,20 @@ import { getAnUser } from "~/services/userService";
 
 const cx = classNames.bind(styles);
 function Profile() {
+
     const { nickname } = useParams();
 
     const [userData, setUserData] = useState();
+    const [tab, setTab] = useState('videos');
+
+    // console.log("tab: " + tab)
 
     useEffect(() => {
         const fetchUserData = async () => {
             try {
                 const res = await getAnUser(nickname);
                 setUserData(res);
+                document.title = `${res?.first_name} ${res?.last_name} (@${res?.nickname})`
             } catch(err) {
                 console.log(err);
             }
@@ -27,14 +31,10 @@ function Profile() {
         fetchUserData();
     }, [nickname]);
 
-    // const { userData } = useAuth();
-    // console.log("userData", userData);
-    // const owner = userData?.nickname === nickname.slice(1);
-    // console.log("owner", owner);
     return (  
         <div className={cx('wrapper')}>
             <ProfileHeader data={userData} />
-            <ProfileTab />
+            <ProfileTab activeTab={tab} setTab={setTab} />
             <CreatorVideo data={userData} />
         </div>
     );
