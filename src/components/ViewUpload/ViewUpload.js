f4186ec8ed6773f4e5a51b7ffdae1f4f18e6bd2f
tiennehihi <vuductientienti@gmail.com>
2025-03-01 12:16:56 +0700

    Upload page UI

---
+++ src/components/ViewUpload/ViewUpload.js
@@ -0,0 +1,87 @@
+import { useRef } from "react";
+import classNames from "classnames/bind";
+import { Toaster, toast } from "react-hot-toast";
+
+import styles from './ViewUpload.module.scss';
+import { UploadIcon } from "~/components/Icons";
+import Button from "~/components/Button";
+
+const cx = classNames.bind(styles);
+function ViewUpload({ onFileSelect }) {
+
+    const inputRef = useRef(null);
+
+    const handleFileInput = (e) => {
+        const file = e.target.files?.[0];
+        if (file && file.type.startsWith("video/")) {
+            onFileSelect(file);
+        } else {
+            toast('Tệp không đúng định dạng.', {
+                position: 'top-center',
+                duration: 3000,
+                style: {
+                    backgroundColor: 'rgba(25, 25, 25, 0.8)',
+                    color: '#fff',
+                    fontWeight: 'italic',
+                    width: '100%',
+                },
+                iconTheme: {
+                    display: 'none',
+                },
+            });
+        }
+    }
+
+    const handleDropVideo = (e) => {
+        e.preventDefault();
+        const file = e.dataTransfer.files?.[0];
+        if (file && file.type.startsWith("video/")) {
+            onFileSelect(file);
+        } else {
+            toast('Tệp không đúng định dạng.', {
+                position: 'top-center',
+                duration: 3000,
+                style: {
+                    backgroundColor: 'rgba(25, 25, 25, 0.8)',
+                    color: '#fff',
+                    fontWeight: 'italic',
+                    width: '100%',
+                },
+                iconTheme: {
+                    display: 'none',
+                },
+            });
+        }
+    }
+
+    return ( 
+        <div className={cx('wrapper')}>
+            <div className={cx('container')} 
+                 onClick={() => inputRef.current.click()}
+                 onDragOver={(e) => e.preventDefault()}
+                 onDrop={handleDropVideo}
+            >
+                <span><UploadIcon width='5rem' height="5rem" /></span>
+                <span className={cx('info')}>Select video to upload</span>
+                <span className={cx('info')}>Or drag and drop a file</span>
+                <span className={cx('info')}>Long videos can be split into multiple parts to get more exposure</span>
+                <span className={cx('note')}>MP4 or WebM</span>
+                <span className={cx('note')}>720x1280 resolution or higher</span>
+                <span className={cx('note')}>Up to 30 minutes</span>
+                <span className={cx('note')}>Less than 2 GB</span>
+                <Button className={cx('upload-btn')} primary>Select file</Button>
+                <input
+                    ref={inputRef}
+                    id="fileInput"
+                    type="file"
+                    accept="video/mp4,video/webm"
+                    style={{ display: "none" }}
+                    onChange={handleFileInput}
+                />
+            </div>
+            <Toaster />
+        </div>
+    );
+}
+
+export default ViewUpload;
--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,13 +1,151 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+import classNames from "classnames/bind";
+
+import { ActionIcon, LiveIcon, MusicNoteIcon, SearchIcon, StaticPauseIcon } from "~/components/Icons";
+import { useAuth } from "~/contexts/AuthContext";
+import ViewUpload from "~/components/ViewUpload";
+import styles from "./Upload.module.scss";
+import Image from "~/components/Images";
+import images from "~/assets/images";
+
+import videos from '~/assets/videos';
+import Button from "~/components/Button";
+
+const cx = classNames.bind(styles);
 function Upload() {
+    const { userData } = useAuth();
+    
     useEffect(() => {
         document.title = 'TikTok Studio';
     }, []);
+
+    const videoRef = useRef(null);
+
+    // useEffect(() => {
+    //     console.log("video: " + videoRef);
+        
+    // }, [videoRef]);
+
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [showIconPause, setShowIconPause] = useState(false);
+    // const [spinAnimate, setSpinAnimate] = useState(false)
+    const [videoUrl, setVideoUrl] = useState('')
+
+    const handleSelectFile = (file) => {
+        setSelectedFile(file);
+        setVideoUrl(URL.createObjectURL(file));
+    };
+    
+    if (selectedFile) {
+        console.log("File details:", { 
+            name: selectedFile.name, 
+            size: selectedFile.size, 
+            type: selectedFile.type, 
+            lastModified: selectedFile.lastModified ,
+            url: videoUrl,
+            length: selectedFile.duration
+        });
+    }
+
+    const handlePlayPause = () => {
+        if (videoRef.current && videoRef.current.paused) {
+            videoRef.current.play();
+            setShowIconPause(false);
+            // setSpinAnimate(true);
+        } else {
+            videoRef.current.pause();
+            setShowIconPause(true);
+            // setSpinAnimate(false);
+        }
+    }
+
     return ( 
-        <div>
-            <div className="content">
-                <h2>Upload page</h2>
-            </div>
+        <div className={cx('wrapper')}>
+            {!selectedFile ? (
+                <ViewUpload onFileSelect={handleSelectFile} />
+            ) : (
+                <div className={cx('wrapper-content')}>
+                    <div className={cx('header')}>
+                        <h1 className={cx('title')}>Upload video</h1>
+                        <p>Post a video to your account</p>
+                    </div>
+                    <div className={cx('container')}>
+                        <div className={cx('video-container')}>
+                            <Image className={cx('bg-image')} src={images.bgIphone} />
+                            <div className={cx('video')}>
+                                <video autoPlay onClick={handlePlayPause} className={cx('video-url')} ref={videoRef} src={videoUrl} loop></video>
+                                {showIconPause && <StaticPauseIcon className={cx('pause-icon')} width='3rem' height='3rem' />}
+                            </div>
+                            <div className={cx('header')}>
+                                <div className={cx('live')}>
+                                    <LiveIcon />
+                                </div>
+                                <div>Following</div>
+                                <div className={cx('active')}>For you</div>
+                                <div className={cx('search')}>
+                                    <SearchIcon />
+                                </div>
+                            </div>
+                            <div className={cx('controls')}>
+                                <Image className={cx('control-icon')} src={images.iconMobile} />
+                            </div>
+                            <div className={cx('actions')}>
+                                <div className={cx('avatar')}>
+                                    <Image className={cx('avatar-image')} src={images.noImage} />
+                                </div>
+                                <ActionIcon />
+                            </div>
+                            <div className={cx('video-content')}>
+                                <div className={cx('full-name')}>{`${userData.first_name} ${userData.last_name}`}</div>
+                                <div className={cx('description')}>{selectedFile.name.slice(0, -4)}</div>
+                                <div className={cx('video-content__footer')}>
+                                    <div className={cx('music')}>
+                                        <MusicNoteIcon width="1.6rem" height="1.6rem" className={cx('music-icon')} /> Original sound - {userData.nickname}
+                                    </div>
+                                    <div className={cx('thumb', 'spin')}>
+                                        <Image className={cx('thumb-image')} src={images.noImage} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx('edit-video')}>
+                            <div className={cx('caption')}>
+                                <p>Caption</p>
+                                <input type="text" value={selectedFile.name.slice(0, -4)} readOnly />
+                            </div>
+                            <div className={cx('cover')}>
+                                <p>Cover</p>
+                                <canvas></canvas>
+                            </div>
+                            <div className={cx('released')}>
+                                <p>Who can view this video</p>
+                                <select name="viewer" id="">
+                                    <option value="public">Public</option>
+                                    <option value="private">Private</option>
+                                    <option value="friends">Friends</option>
+                                </select>
+                            </div>
+                            <div className={cx('allowable')}>
+                                <input type="checkbox" name="comment" id="" />
+                                <label>Comments</label>
+                                <input type="checkbox" name="duet" id="" />
+                                <label>Duet</label>
+                            </div>
+                            <div className={cx('copyright')}>
+                                <p>Run a copyright check</p>
+                                <input type="checkbox" name="copyright" id="" />
+                                <p className={cx('copyright-note')}>
+                                    We'll check your video for potential copyright infringements on used sounds. If infringements are found, you can edit the video before posting.<strong>Learn more</strong>
+                                </p>
+                            </div>
+                            <div className={cx('action')}>
+                                <button>Discard</button>
+                                <Button primary>Upload</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
      );
 }
